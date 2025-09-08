c2801ebf111f37ed5537f12b2de146083500b4e3
ShErWooD-ForEsd <sherwooe@otc.edu>
2025-09-07 21:04:24 -0500

    I was unable to get this working. Need help piecing this together. I am just not easily understanding how to construct a lot of these projects.

--- slot_game.php
+++ slot_game.php
@@ -1,50 +1,42 @@
 <?php
-$letters = ['A', 'B', 'C'];
-$results_list = [];
-$result = "";
-$credits = "";
+$letters = ['A', 'B', 'C',];
+// $results_list = [];
+// $result = "";
+// $credits = 0;
 
-while ($o_counter <= 10 || $credits = 500) {
+while ($o_counter <= 20 || $credits = 500) {
     // $o_counter++;
+    $roll_array = [];
 
     for ($i_counter = 1; $i_counter <= 3; $i_counter++) {
+        // $roll_array = [];
         $random_key = array_rand($letters);
         $random_letters = $letters[$random_key];
         $result = $random_letters;
         echo $result;
+        // array_push($roll_array, $result . $i_counter);
+        // echo $roll_array;
     };
+    echo "<br>The roll is: $result";
+    
 
 
     // $results_list[] = $result . $i_counter;
 
 
-    $credits = match($results) {
+    $credits = match ($results) {
         'AAA', 'BBB', 'CCC' => 100,
         'AAB', 'ABA', 'BAA', 'ABB', 'BBA', 'BAB', 'BCC', 'CBC', 'CCB', 'ACC', 'CAC', 'CCA' => 50,
         'Any Other Combination' => 0
-    }; echo $credits;
+    };
+    echo $credits;
 }
 
-    // foreach ($results_a as $key) {
-    //     echo $item . "<br>";
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// foreach ($results as $key) {
+//     echo $result . "<br>";
+// }
 
 
 
 ?>
+

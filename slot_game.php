45d1ce9e351b7424d5656ef5674378fe89104df0
ShErWooD-ForEsd <sherwooe@otc.edu>
2025-09-17 18:34:43 -0500

    finally finished this. I had to totally restart and rebuild from the inside out. I built the for loop first with the match, and then built the while loop around it. This helped me a lot, and I will probably try from the inside out when I am stuck with loops

--- slot_game.php
+++ slot_game.php
@@ -1,21 +1,25 @@
 <?php
-
 $letters_array = ["A", "B", "C"];
-
+$spins = 0;
+$credits = 0;
+$total_credits = 0;
+while ($spins < 10 && $total_credits <= 500) {
+    static $total_credits = 0;
+    $spins++;
     $results_array = [];
-    $credits = 0;
     for ($i = 1; $i <= 3; $i++) {
         $random_key = array_rand($letters_array);
         $random_letters = $letters_array[$random_key];
         $results_array[] .= $random_letters;
     }
     $three_letters = $results_array[0] . $results_array[1] . $results_array[2];
-
     $credits = match ($three_letters) {
         'AAA', 'BBB', 'CCC' => 100,
         'AAB', 'ABA', 'BAA', 'ABB', 'BBA', 'BAB', 'BCC', 'CBC', 'CCB', 'ACC', 'CAC', 'CCA' => 50,
         default => 0
     };
-    echo $three_letters . " ---> " . "$" . $credits;
-
-    ?>
+    $total_credits += $credits;
+    echo $three_letters . " ---> " . "$" . $credits . "<br>";
+}
+echo ("<br>GAME OVER. Total winnings is " . $total_credits);
+?>
